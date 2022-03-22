80f955ecf3f8e112a3fcd9231ae6d1bd3fe711cd
thabie-dev <taniatdube1@gmail.com>
2022-03-22 05:06:35 +0200

    highlight

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const sentenceElem = document.querySelector(".sentence");
 const wordCountElem = document.querySelector(".wordCount");
 const maxWordCountElem = document.querySelector(".maxWordCount");
 const errorMessageElem = document.querySelector(".errorMessage");
-
-
+const buttonElem =document.querySelector(".analyse-btn");
+const outputSentenceElem =document.querySelector(".output-sentence");
+const hiddenWordsElem =document.querySelector(".hidden-words");
 
 
 document.addEventListener('DOMContentLoaded', ()=> {
@@ -22,17 +23,39 @@ const elements = selectors.map((selectors) =>DocumentTimeline.querySelector);
         errorMessage,
     ] = elements
 
- button.addEventListener('click', () => {
-     let sentence =input.value;
+ buttonElem.addEventListener('click', () => {
+     let sentence =sentenceElem.value;
+     let count =maxWordCountElem.value;
      sentence =sentence.trim();
-     const words = sentence.split();
-     sentence = word.replace();
-     if (Chars.length > 5){
-         word = `<mark>${word}</mark>`;
+     var outputSentence = "";
+     console.log(sentence, count);
+     const words = sentence.split(" ");
+     console.log(words.length);
+     if (count > words.length){
+         errorMessageElem.innerHTML = 'Soorry, this is too low!';
      }
-     return word
+     //var hiddenWords = hiddenWordsElem;
+      // console.log(hiddenWords.checked);
+
+     wordCountElem.innerHTML= words.length;
+     for (var i = 0; i < words.length; i++){
+        var word = words[i];
+        
+        if (word.length > 4){
+           word = '<div class="highlight">'+word+'</div>'
+        }
+        outputSentence += word+' ';
+        outputSentenceElem.innerHTML = outputSentence;
+     }
+
+     //sentence = word.replace();
+     //if (Chars.length > 5){
+        // word = `<mark>${word}</mark>`;
+    // }
+    // return word
     })
-    outputSentence.innerHTML =sentence.join('')
-    wordCount.innerHTML =`Word count: $words.length`
+   // outputSentence.innerHTML =sentence.join('')
+    //wordCount.innerHTML =`Word count: $words.length`
+    
 
 })
